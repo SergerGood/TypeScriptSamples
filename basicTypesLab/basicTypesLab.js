fabe67591f8f7ebf46aa379036e7f51391ad5a70
SergerGood <serzher@inbox.ru>
2017-08-16 22:14:15 +0300

    Declare Types

---
+++ basicTypesLab/basicTypesLab.js
@@ -0,0 +1,8 @@
+var usedColor = "green";
+var squareSizeNumber = 100;
+var squareSize = squareSizeNumber + "px";
+var button = document.createElement("button");
+button.textContent = "Change Color";
+document.body.appendChild(button);
+var div = document.createElement("div");
+document.body.appendChild(div);
---
+++ basicTypesLab/basicTypesLab.ts
@@ -0,0 +1,10 @@
+let usedColor: string = "green";
+let squareSizeNumber: number = 100;
+let squareSize: string = `${ squareSizeNumber }px`;
+
+let button: Element = document.createElement("button");
+button.textContent = "Change Color";
+document.body.appendChild(button);
+
+let div: Element = document.createElement("div");
+document.body.appendChild(div);
