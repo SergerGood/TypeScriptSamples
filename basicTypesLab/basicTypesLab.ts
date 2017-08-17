a68a9349ddb332945c64845eaf57590a19f41a1e
SergerGood <serzher@inbox.ru>
2017-08-17 23:20:57 +0300

    buld task

--- basicTypesLab/basicTypesLab.ts
+++ basicTypesLab/basicTypesLab.ts
@@ -1,19 +1,6 @@
-class colorChange{
-    div: Element;
-
-    constructor(div: Element){
-        this.div = div;
-    }
-
-    change(color: string): boolean {
-        (this.div as HTMLElement).style.backgroundColor = color;
-        return true;
-    }
-}
-
 let usedColor: string = "green";
 let squareSizeNumber: number = 100;
-let squareSize: string = `${ squareSizeNumber }px`;
+let squareSize: string = `${squareSizeNumber}px`;
 
 let button: Element = document.createElement("button");
 button.textContent = "Change Color";
@@ -27,7 +14,7 @@ document.body.appendChild(div);
 
 let colorChange: Function = (element: Element, color: string): boolean => {
     (<HTMLElement>element).style.backgroundColor = color;
-    return true; 
+    return true;
 }
 
 (button as HTMLElement).onclick = () => {
--- basicTypesLab/exercise_2.ts
+++ basicTypesLab/exercise_2.ts
@@ -1,7 +1,7 @@
-class colorChange2{
+class colorChange2 {
     div: Element;
 
-    constructor(div: Element){
+    constructor(div: Element) {
         this.div = div;
     }
 
@@ -11,15 +11,15 @@ class colorChange2{
     }
 }
 
-interface elementSet{
+interface elementSet {
     "div": Element,
     "button": Element
 }
 
-enum Colors{ Green, Red, Blue, Black }
+enum Colors { Green, Red, Blue, Black }
 
 let squareSizeNumber2: number = 100;
-let squareSize2: string = `${ squareSizeNumber2 }px`;
+let squareSize2: string = `${squareSizeNumber2}px`;
 let elementSets: Array<elementSet> = [];
 let Elements = {
     "div": document.createElement("div"),
@@ -30,21 +30,20 @@ for (let index: number = 0; index < 4; index++) {
     elementSets.push({
         "div": document.createElement("div"),
         "button": document.createElement("button")
-    });    
+    });
 }
 
 elementSets.map((element, index) => {
     let colorChange = new colorChange2(element.div);
-    
+
     (element.div as HTMLElement).style.width = squareSize2;
     (element.div as HTMLElement).style.height = squareSize2;
-    
+
     element.button.textContent = "Change Color";
-    (element.button as HTMLElement).onclick = (event) =>{
+    (element.button as HTMLElement).onclick = (event) => {
         colorChange.change(Colors[index]);
     }
-    
+
     document.body.appendChild(element.div);
     document.body.appendChild(element.button);
 });
-    
