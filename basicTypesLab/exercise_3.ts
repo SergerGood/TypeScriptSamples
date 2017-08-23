dbc59913e98659ea4a0cbccd49c86c52e17d55fb
SergerGood <serzher@inbox.ru>
2017-08-23 22:13:15 +0300

    Overloaded Method

--- basicTypesLab/exercise_3.ts
+++ basicTypesLab/exercise_3.ts
@@ -5,8 +5,29 @@ class colorChange3 {
         this.div = div;
     }
 
-    change(color: string): boolean {
+    change(color: string | number): boolean {
+        if (typeof(color) === "number"){
+            return true;
+        }
+
         (this.div as HTMLElement).style.backgroundColor = color;
+        return true;
+    }
+}
+
+enum Colors3 { Green, Red, Blue, Black }
+
+class numericColor extends colorChange3 {
+    static Colors3 = Colors3;
+
+    constructor(div: Element){
+        super(div);
+        (this.div as HTMLElement).style.width = squareSize3;
+        (this.div as HTMLElement).style.height = squareSize3;        
+    }
+
+    change(color: number): boolean {
+        (this.div as HTMLElement).style.backgroundColor = Colors3[color];
         return true;
     }
 }
@@ -14,18 +35,6 @@ class colorChange3 {
 interface elementSet {
     "div": Element,
     "button": Element
-}
-
-enum Colors3 { Green, Red, Blue, Black }
-
-class numericColor extends colorChange3 {
-    static Colors = Colors3;
-
-    constructor(div: Element){
-        super(div);
-        (this.div as HTMLElement).style.width = squareSize3;
-        (this.div as HTMLElement).style.height = squareSize3;        
-    }
 }
 
 let squareSizeNumber3: number = 100;
@@ -39,15 +48,23 @@ for (let index: number = 0; index < 4; index++) {
     });
 }
 
+let getRandomInt: Function = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 elementSets3.map((element, index) => {
-    let colorChange = new colorChange3(element.div);
+    let colorChange = new numericColor(element.div);
 
     (element.div as HTMLElement).style.width = squareSize3;
     (element.div as HTMLElement).style.height = squareSize3;
 
     element.button.textContent = "Change Color";
     (element.button as HTMLElement).onclick = (event) => {
-        colorChange.change(Colors3[index]);
+        let color = getRandomInt(0, 3);
+        colorChange.change(color);
     }
 
     document.body.appendChild(element.div);
