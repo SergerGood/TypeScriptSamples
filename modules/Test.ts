435b6f04582cefb12ac9b15524644ac9044987e6
SergerGood <serzher@inbox.ru>
2017-09-03 22:28:14 +0300

    Ambient Modules

--- modules/Test.ts
+++ modules/Test.ts
@@ -1,5 +1,9 @@
+///<reference path="node.d.ts"/>
+
 import validator from "./defaultCodeValidator";
 import validateFunction from "./zipCodeValidator";
+
+import * as URL from "url" 
 
 let myValidator = new validator();
 let result = myValidator.isAccept("asd");
@@ -8,3 +12,5 @@ let strings = ["1", "2", "3"];
 strings.forEach(s =>{
     console.log(`"${s}" ${validateFunction.apply(s) ? "match" : "does not match"}`);
 });
+
+let myUrl = URL.parse("http://www.google.com");
