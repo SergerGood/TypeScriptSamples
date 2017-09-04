4b7f31b428102ac76ebbd06e2dab562c4b3f87af
SergerGood <serzher@inbox.ru>
2017-09-04 22:11:38 +0300

    demo modules

---
+++ modules/module1.ts
@@ -0,0 +1,11 @@
+export let someString: string = "string"
+
+export let someMethod = () => { }
+
+class someClass { 
+    someProperty: string = "string";
+}
+
+export interface someInterface { }
+
+export default new someClass;
---
+++ modules/module2.ts
@@ -0,0 +1,5 @@
+import someClass from './module1'
+
+import { someInterface, someMethod } from './module1'
+
+let instanceOfClass = someClass;
