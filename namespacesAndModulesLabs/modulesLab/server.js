49bd0312527944d12a5a1a2122fa607152319954
SergerGood <serzher@inbox.ru>
2017-09-12 22:52:07 +0300

    CommonJS With SystemJS

--- namespacesAndModulesLabs/modulesLab/server.js
+++ namespacesAndModulesLabs/modulesLab/server.js
@@ -3,7 +3,7 @@ var express = require('express');
 
 var app = express();
 
-var staticPath = path.join(__dirname, '/../../out/namespacesAndModulesLabs/modulesLab/');
+var staticPath = path.join(__dirname, '/');
 app.use(express.static(staticPath));
 
 app.listen(3000, function(){
