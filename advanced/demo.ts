283d705a568220256662592048388696640b5373
SergerGood <serzher@inbox.ru>
2017-09-18 22:31:16 +0300

    Intersection and union

--- advanced/demo.ts
+++ advanced/demo.ts
@@ -9,5 +9,26 @@ namespace Advanced {
         console.log(key);//0, 1, 2
     }
 
+    function padLeft(value: string, padding: string | number) {
+        if (typeof padding === "number") { }
+        if (typeof padding === "string") { }
+    }
 
+    interface Bird {
+        fly();
+        layEggs();
+    }
+
+    interface Fish {
+        swim();
+        layEggs();
+    }
+
+    function getSmallPet(): Fish | Bird {
+        return null;
+    }
+
+    let pet = getSmallPet();
+    pet.layEggs();
+    //pet.fly(); ERROR
 }
